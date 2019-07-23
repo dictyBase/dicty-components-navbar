a99a7bd9c12c3cbf4fe7d7288f4ae4305b3e25e1
wildlifehexagon <crossthebreeze@gmail.com>
2019-07-23 18:04:49 -0500

    refactor: replace styled-components ThemeProvider with mui version

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,20 @@
 // @flow
-import React, { Component } from 'react'
-import styled, { ThemeProvider } from 'styled-components'
-import Brand from './Brand'
-import Dropdown from './Dropdown'
-import Link from './Link'
-import MenuIcon from './MenuIcon'
-import { transitionToAuto, transitionFromAuto, wasClicked } from '../utils/dom'
+import React, { Component } from "react"
+import { ThemeProvider } from "@material-ui/styles"
+import styled from "styled-components"
+import Brand from "./Brand"
+import Dropdown from "./Dropdown"
+import Link from "./Link"
+import MenuIcon from "./MenuIcon"
+import { transitionToAuto, transitionFromAuto, wasClicked } from "../utils/dom"
 
 const Container = styled.div`
   width: 100%;
   z-index: 10000;
 
   @media (max-width: 768px) {
-    ${''/* overflow-y: ${ props => props.open ? 'hidden' : 'visible' }; */}
     overflow: hidden;
     position: initial;
-    ${''/* height: ${ props => props.open ? 'auto' : '50px' }; */}
     height: 50px;
     transition: height 0.3s ease;
     -ms-overflow-style: none;
@@ -29,9 +28,11 @@ const Nav = styled.nav`
   display: flex;
   flex-direction: row;
   flex-wrap: nowrap;
-  background: ${ props => props.theme.primary ? props.theme.primary : '#15317e' };
-  color: ${ props => props.theme.text ? props.theme.text : 'white' };
-  min-height: ${ props => props.theme.height ? props.theme.height + 'px' : '50px' };
+  background: ${props =>
+    props.theme.primary ? props.theme.primary : "#15317e"};
+  color: ${props => (props.theme.text ? props.theme.text : "white")};
+  min-height: ${props =>
+    props.theme.height ? props.theme.height + "px" : "50px"};
 
   @media (max-width: 768px) {
     position: relative;
@@ -64,152 +65,142 @@ const Header = styled.li`
     display: flex;
     flex-direction: column;
     align-items: center;
-    ${''/* width: ${ props => props.maxWidth ? props.maxWidth + 'px' : '200px' }; */}
     z-index: 10;
     width: 100%;
   }
 `
 
 type Props = {
-    items: Array<Object>,
-    brand: Object,
-    theme: Object
+  items: Array<Object>,
+  brand: Object,
+  theme: Object,
 }
 type State = {
-    activeIndex: number,
-    open: boolean
+  activeIndex: number,
+  open: boolean,
 }
 
 export default class Navbar extends Component {
-    displayName = 'Navbar'
-    props: Props
-    state: State
-    nav: any
-    icon: any
-    constructor() {
-        super()
-        this.state = {
-            activeIndex: -1,
-            open: false
-        }
+  displayName = "Navbar"
+  props: Props
+  state: State
+  nav: any
+  icon: any
+  constructor() {
+    super()
+    this.state = {
+      activeIndex: -1,
+      open: false,
     }
-    componentDidMount() {
-        document.addEventListener('click', this.handleDocumentClick)
-        // Necessary to allow container to expand to accomodate open dropdowns
-        this.container.addEventListener('transitionend', this.handleTransitionend)
-        this.calcBreakpoint()
+  }
+  componentDidMount() {
+    document.addEventListener("click", this.handleDocumentClick)
+    // Necessary to allow container to expand to accomodate open dropdowns
+    this.container.addEventListener("transitionend", this.handleTransitionend)
+  }
+  handleTransitionend = e => {
+    const { open } = this.state
+    if (open && e.propertyName === "height") {
+      this.container.style.height = "auto"
     }
-    handleTransitionend = (e) => {
-        const { open } = this.state
-        if (open && e.propertyName === 'height') {
-            this.container.style.height = 'auto'
-        }
+  }
+  handleDocumentClick = (e: MouseEvent) => {
+    const { open } = this.state
+    if (!wasClicked(e, this.nav) && open) {
+      this.close()
     }
-    handleDocumentClick = (e: MouseEvent) => {
-        const { open } = this.state
-        if (!wasClicked(e, this.nav) && open) {
-            this.close()
-        }
+  }
+  toggle = (e: SyntheticEvent) => {
+    const { open } = this.state
+
+    e.nativeEvent.stopImmediatePropagation()
+    e.preventDefault()
+    if (open) {
+      this.close()
+    } else {
+      this.open()
     }
-    calcBreakpoint = () => {
-        // const { items } = this.props
-        // const text = items.reduce((acc, curr) => {
-        //     console.log(acc)
-        //     return acc + curr.title
-        // })
-        // const padding = items.reduce((acc, curr) => {
-        //     // console.log(acc)
-        //     if (curr.dropdown) {
-        //         return acc + 30
-        //     } else {
-        //         return acc + 20
-        //     }
-        // })
-        // let text = ''
-        // for (let i = 0; i < items.length; i++) {
-        //     let item = items[i]
-        //     if (!item.element) {
-        //         text += item.title
-        //     }
-        // }
-        // const textWidth = calcTextWidth(text, '16px', 'Roboto sans-serif')
-        // console.log(textWidth + padding)
-    }
-    toggle = (e: SyntheticEvent) => {
-        const { open } = this.state
-        e.nativeEvent.stopImmediatePropagation()
-        e.preventDefault()
-        if (open) {
-            this.close()
-        } else {
-            this.open()
-        }
-    }
-    close = () => {
-        this.setState({
-            open: false,
-            activeIndex: -1
+  }
+  close = () => {
+    this.setState({
+      open: false,
+      activeIndex: -1,
+    })
+    transitionFromAuto(this.container, 50)
+  }
+  open = () => {
+    this.setState({
+      open: true,
+    })
+    transitionToAuto(this.container)
+  }
+  changeDropdown = (i: number) => {
+    this.setState({
+      activeIndex: i,
+    })
+  }
+  renderBrand = () => {
+    const { title, href } = this.props.brand
+    return <Brand title={title} href={href} />
+  }
+  renderItems = () => {
+    const { activeIndex, open } = this.state
+    let { items } = this.props
+    items = items.map((item, i) => {
+      if (item.element) {
+        return React.cloneElement(item.element, {
+          ...item.element.props,
+          key: i,
         })
-        transitionFromAuto(this.container, 50)
-    }
-    open = () => {
-        this.setState({
-            open: true
-        })
-        transitionToAuto(this.container)
-    }
-    changeDropdown = (i: number) => {
-        this.setState({
-            activeIndex: i
-        })
-    }
-    renderBrand = () => {
-        const { title, href } = this.props.brand
-        return <Brand title={ title } href={ href } />
-    }
-    renderItems = () => {
-        const { activeIndex, open } = this.state
-        let { items } = this.props
-        items = items.map((item, i) => {
-            if (item.element) {
-                return React.cloneElement(item.element, { ...item.element.props, key: i })
-            } else if (item.dropdown) {
-                return (
-                    <Dropdown
-                      key={ i }
-                      index={ i }
-                      open={ activeIndex === i ? true : false }
-                      items={ item.items }
-                      title={ item.title }
-                      changeDropdown={ this.changeDropdown }
-                      controlled={ true }
-                    />
-                )
-            } else {
-                return <Link key={ i } href={ item.href } title={ item.title } />
-            }
-        })
-        return <Items open={ open }>{ items }</Items>
-    }
-    componentWillUnmount() {
-        document.removeEventListener('click', this.handleDocumentClick)
-        this.container.removeEventListener('transitionend', this.handleTransitionend)
-    }
-    render() {
-        const { theme, brand, items } = this.props
-        const { open } = this.state
+      } else if (item.dropdown) {
         return (
-            <ThemeProvider theme={ theme ? theme : {} }>
-              <Container open={ open } items={ items } brand={ brand } innerRef={ el => this.container = el}>
-                <Nav open={ open } innerRef={ el => this.nav = el }>
-                  <Header open={ open }>
-                    <MenuIcon ref={ el => this.icon = el } onClick={ this.toggle } open={ open } />
-                    { brand && this.renderBrand() }
-                  </Header>
-                  { items && this.renderItems() }
-                </Nav>
-              </Container>
-            </ThemeProvider>
+          <Dropdown
+            key={i}
+            index={i}
+            open={activeIndex === i ? true : false}
+            items={item.items}
+            title={item.title}
+            changeDropdown={this.changeDropdown}
+            controlled={true}
+          />
         )
-    }
+      } else {
+        return <Link key={i} href={item.href} title={item.title} />
+      }
+    })
+    return <Items open={open}>{items}</Items>
+  }
+  componentWillUnmount() {
+    document.removeEventListener("click", this.handleDocumentClick)
+    this.container.removeEventListener(
+      "transitionend",
+      this.handleTransitionend,
+    )
+  }
+  render() {
+    const { theme, brand, items } = this.props
+    const { open } = this.state
+
+    return (
+      <ThemeProvider theme={theme ? theme : {}}>
+        <Container
+          open={open}
+          items={items}
+          brand={brand}
+          innerRef={el => (this.container = el)}>
+          <Nav open={open} innerRef={el => (this.nav = el)}>
+            <Header open={open}>
+              <MenuIcon
+                onClick={this.toggle}
+                open={open}
+                ref={el => (this.icon = el)}
+              />
+              {brand && this.renderBrand()}
+            </Header>
+            {items && this.renderItems()}
+          </Nav>
+        </Container>
+      </ThemeProvider>
+    )
+  }
 }
